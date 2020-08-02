70a41f14063ed9fbf05fd9a0e5a9f83a67781756
Ron Harlev <harlev@gmail.com>
2020-08-01 21:18:47 -0700

    add caching

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const port = 3000
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 
-app.use(express.static('public'))
+var options = {
+  etag: false,
+  setHeaders: function (res, path, stat) {
+    setLongTermCache(res)
+  }
+}
+
+app.use(express.static('public', options))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+
+function setLongTermCache(res) {
+  const date = new Date();
+  date.setFullYear(date.getFullYear() + 1);
+  res.setHeader("Expires", date.toUTCString());
+  res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
+}
